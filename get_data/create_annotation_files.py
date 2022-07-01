5dc39066a06e8050ac6296541238247553e31aec
Shivvrat Arya <shivvarya@gmail.com>
2022-07-01 16:56:26 +0530

    Why:
    * Update dataloader so that we can get labels for yolo
    
    This change addresses the need by:
    
    *

---
+++ get_data/create_annotation_files.py
@@ -0,0 +1,4 @@
+
+def get_frame_path(participant_id, video_id, frame_num):
+    path = f"/home/sxa180157/ptg/epic_kitchens_100/object_detection_frames/EPIC-KITCHENS/{participant_id}/object_detection_labels/{video_id}/{frame_num}.txt"
+    return path
--- utils/dataloaders.py
+++ utils/dataloaders.py
@@ -390,7 +390,7 @@ class LoadStreams:
 
 def img2label_paths(img_paths):
     # Define label paths as a function of image paths
-    sa, sb = f'{os.sep}images{os.sep}', f'{os.sep}labels{os.sep}'  # /images/, /labels/ substrings
+    sa, sb = f'{os.sep}object_detection_images{os.sep}', f'{os.sep}object_detection_labels{os.sep}'  # /images/, /labels/ substrings
     return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
 
 
