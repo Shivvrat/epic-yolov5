664ac21fa39dee4847b361bccc423edd3c11375c
Shivvrat Arya <shivvarya@gmail.com>
2022-07-12 10:44:17 +0530

    Why: * Increase the batch size
    This change addresses the need by:
    *

--- train.sh
+++ train.sh
@@ -2,6 +2,6 @@ eval "$(conda shell.bash hook)"
 
 conda activate /home/sxa180157/anaconda3/envs/epic_yolo
 
-python train.py --img 1280 --batch 32 --epochs 20 --data get_data/epic_kitchens.yaml --weights yolov5m.pt \
+python train.py --img 1280 --batch 64 --epochs 20 --data get_data/epic_kitchens.yaml --weights yolov5m.pt \
 --cache disk
 #makes it faster
