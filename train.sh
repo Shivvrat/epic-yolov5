887c8c445d9f8f062e3982f75eda9603d4688a41
Shivvrat Arya <shivvarya@gmail.com>
2022-07-13 09:52:25 +0530

    Why: * Resume the training from previous image
    This change addresses the need by:
    *

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ eval "$(conda shell.bash hook)"
 conda activate /home/sxa180157/anaconda3/envs/epic_yolo
 
 python train.py --img 1280 --batch 64 --epochs 20 --data get_data/epic_kitchens.yaml --weights yolov5m.pt \
---cache disk
+--cache disk --resume True
 #makes it faster
